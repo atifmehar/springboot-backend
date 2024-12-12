6a464a767437382abf5b20ab4203e85fe69fbd6c
atif mehar <atifmehar@hotmail.com>
2024-12-12 14:46:11 -0600

    added project files

---
+++ src/main/java/net/javaguides/springboot/SpringbootBackendApplication.java
@@ -0,0 +1,14 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		// this is dummy commit
+		SpringApplication.run(SpringbootBackendApplication.class, args);// ssss
+	}
+
+}
